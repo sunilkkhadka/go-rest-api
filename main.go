72bedd5ae8115e659d984cf5bb18bed35d2c4d1a
Sunil Khadka <sunil.khadka@machnetinc.com>
2023-03-01 20:24:49 +0545

    T-2: Added routes, es > created server > created handler function

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+var profiles []Profile = []Profile{}
 
 type User struct{
 	FirstName string `json:"firstName"`
@@ -19,8 +22,24 @@ type Profile struct {
 	Employee User `json:"employee"`
 }
 
+func addItem(q http.ResponseWriter, r *http.Request){
+	var newProfile Profile
+	json.NewDecoder(r.Body).Decode(&newProfile)
+
+	q.Header().Set("Content-Type", "application/json")
+
+	profiles = append(profiles, newProfile)
+
+	json.NewEncoder(q).Encode(profiles)
+}
+
+
 func main(){
 	router := mux.NewRouter()
+
+	router.HandleFunc("/profiles", addItem).Methods("POST")
+
+	fmt.Println("Server started...")
 	http.ListenAndServe(":5000", router)
-	fmt.Println("Starting server...")
 }
+
