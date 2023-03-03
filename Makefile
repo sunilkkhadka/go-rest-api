d412e499809408e6b033742e9a34f254f1ead017
Sunil Khadka <sunil.khadka@machnetinc.com>
2023-03-03 19:20:34 +0545

    T-3: Created tests and api file for the project

---
+++ Makefile
@@ -0,0 +1,10 @@
+build:
+	@go build -o bin/gobank
+
+run: build
+	@bin/gobank
+
+test:
+	@go test -v ./...
+
+
--- main.go
+++ main.go
@@ -1,45 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
-
-var profiles []Profile = []Profile{}
-
-type User struct{
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
-	Email string `json:"email"`
-}
-
-type Profile struct {
-	Department string `json:"department"`
-	Designation string `json:"designation"`
-	Employee User `json:"employee"`
-}
-
-func addItem(q http.ResponseWriter, r *http.Request){
-	var newProfile Profile
-	json.NewDecoder(r.Body).Decode(&newProfile)
-
-	q.Header().Set("Content-Type", "application/json")
-
-	profiles = append(profiles, newProfile)
-
-	json.NewEncoder(q).Encode(profiles)
-}
-
+import "fmt"
 
 func main(){
-	router := mux.NewRouter()
-
-	router.HandleFunc("/profiles", addItem).Methods("POST")
-
-	fmt.Println("Server started...")
-	http.ListenAndServe(":5000", router)
+	fmt.Println("Hello world")
 }
-
